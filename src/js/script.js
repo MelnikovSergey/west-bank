c4a961c6ab832f302bcee3f0730f560a76354985
unknown <sergeyandmelnikov@gmail.com>
2022-08-05 04:08:45 +0300

    restart project

--- src/js/script.js
+++ src/js/script.js
@@ -1,162 +1,17 @@
-/*
-function gamePlay(){
-	function personSoot(){
-		if(shoot == '10'){
-			personAnimate(person1);
-			clearInterval(personEnd);
-		}else{
-			alert("Game Over")
-			clearInterval(personEnd);
-		}
-	}		
-	var personEnd = setInterval(personSoot, 2000);
-	closeDoor(elem1);
+(function(id) {
 
-};
-
-$(function(){
-var elem1 = document.getElementById('sprite_1');
-var elem2 = document.getElementById('sprite_2');
-var elem3 = document.getElementById('sprite_3');
-var person1 = document.getElementById('people_1');
-openDoor(elem1);
-personAnimate(person1);
-closeDoor(elem1);
-}); 
-*/
-
-
-
-window.onload = function() {
-	var door = 'close';
-	var elemsDoor;
-	var gamecount = 0;
-	var bankirLife = 3; 
-
-	function getRandomInt(min, max) {
-		return Math.floor(Math.random() * (max - min)) + min;
-	}
-
-	function openDoors() {
-
-		var randomDoor = getRandomInt(1, 3);	
-
-		switch(randomDoor) {
-			case 1:
-			openDoor(elem1);
-			openDoor(elem1);
-			elemsDoor = 'openelem1'; 
-			break;
-
-			case 2:
-			openDoor(elem2);
-			openDoor(elem2);
-			elemsDoor = 'openelem2';
-			break;	
-
-			case 3:
-			openDoor(elem3);
-			openDoor(elem3);
-			elemsDoor = 'openelem3';
-			break;	
-	
-			case 4:
-			openDoor(elem1);
-			elemsDoor = 'openelem1';
-			openDoor(elem2);
-			elemsDoor = 'openelem2';
-			break;
-
-			case 5:
-			openDoor(elem2);
-			elemsDoor = 'openelem2';
-			openDoor(elem3);
-			elemsDoor = 'openelem3';
-			break;
-
-			case 6:
-			openDoor(elem1);
-			elemsDoor = 'openelem1';
-			openDoor(elem3);
-			elemsDoor = 'openelem3';
-			break;				
-		}
-
-		gamecount++;
-	}
-
-	function people() {
-		var people;
-		var randomPeople = getRandomInt(1, 5);	
-
-		switch(randomPeople) {
-			case 1:
-			people = 'bandit';
-
-			people_1.classList.add('bandit');
-			people_2.classList.add('bandit');
-			people_3.classList.add('bandit');
-			console.log('bandit');
-			break;
-
-			case 2:
-			people = 'man';
-
-			people_1.classList.add('man');
-			people_2.classList.add('man');
-			people_3.classList.add('man');
-			console.log('man');
-			break;	
-
-			case 3:
-			people = 'woman'; 
-
-			people_1.classList.add('woman');
-			people_2.classList.add('woman');
-			people_3.classList.add('woman');
-			console.log('woman');
-			break;	
-	
-			case 4:
-			people = 'sexBomBoy';
-
-			people_1.classList.add('sexbomboy');
-			people_2.classList.add('sexbomboy');
-			people_3.classList.add('sexbomboy');
-			console.log('sexBomBoy');
-			break;
-
-			default:
-			people = 'manWithBandit';
-
-			people_1.classList.add('sexbomboy');
-			people_2.classList.add('sexbomboy');
-			people_3.classList.add('sexbomboy');
-			console.log('manWithBandit'); 
-		}
-	}
-
-	// анимируем открытие двери
 	function openDoor(elem){
-		var y = 0;
-
-		function frame(){
-			y -= 186;
-			elem.style.backgroundPositionY = y + 'px';
-			if(y == -186*3){
-				clearInterval(timer);
-			}
+		if(elem.length) {
+		  for(var i = 0; i < elem.length; i++) {
+		    make(elem[i]);
+		  }
+		} else {
+		  make(elem);
 		}
-		var timer = setInterval(frame, 80);
-	}
 
-/*
-	// пытаемся сделать универсальную функцию анимации
-	function openDoor(elem){
-		console.log(door);
-		if (door !== 'open') {
+		function make(elem) {
 			var y = 0;
-
+	
 			function frame(){
 				y -= 186;
 				elem.style.backgroundPositionY = y + 'px';
@@ -164,28 +19,23 @@ window.onload = function() {
 					clearInterval(timer);
 				}
 			}
-			var timer = setInterval(frame, 80);
-			door = 'open';
-			console.log(door);
-
-		} else {
-			function frame(){
-				y += 186;
-				elem.style.backgroundPositionY = y + 'px';
-				if(y == 0){
-					clearInterval(timer);
-				}
-			}
-			var timer = setInterval(frame, 800);
-			door = 'close';
-			console.log(door);
+			var timer = setInterval(frame, 100);
 		}
 	}
-*/
-		// анимируем закрытие двери
-		function closeDoor(elem){
-			var y = -372;
 
+	function closeDoor(elem){
+
+		if(elem.length) {
+		  for(var i = 0; i < elem.length; i++) {
+		    make(elem[i]);
+		  }
+		} else {
+		  make(elem);
+		}
+
+		function make(elem) {
+			var y = -372;
+		
 			function frame(){
 				y += 186;
 				elem.style.backgroundPositionY = y + 'px';
@@ -195,90 +45,145 @@ window.onload = function() {
 			}
 			var timer = setInterval(frame, 100);
 		}
+	}
+
+	function getRandomInt(min, max) {
+		return Math.floor(Math.random() * (max - min)) + min;
+	}
  
-	var elem1 = document.getElementById('sprite_1');
-	var elem2 = document.getElementById('sprite_2');
-	var elem3 = document.getElementById('sprite_3');
+	function selectRandomDoors() {
+		selectDoorsNumber = getRandomInt(0, 6);
+		return selectDoorsNumber;
+	}
 
-	var people_1 = document.getElementById('people_1');
-	var people_2 = document.getElementById('people_2');
-	var people_3 = document.getElementById('people_3');	
+	function openSelectDoors(selectDoorsNumber) {
+		openDoor(doorsArray[selectDoorsNumber]);
+	}
 
-	function personAnimate(person){
-		var x = 0;
+	function closeSelectDoors(selectDoorsNumber) {
+		closeDoor(doorsArray[selectDoorsNumber]);
+	}
+
+	function selectRandomCustomer() {
+		var randomCustomer = getRandomInt(0, 6);
+		return randomCustomer;	
+	}
+
+	function createCustomersArray() {
+		for(var i = 0; i < 3; i++) {
+			roundCustomersArray[i] = customersArray[selectRandomCustomer()];
+		}
+	}	
+
+	function uploadCustomers() {
+		createCustomersArray();
+
+		customer1.className += " " + roundCustomersArray[1];
+		customer2.className += " " + roundCustomersArray[2];
+		customer3.className += " " + roundCustomersArray[3];
+	}
+
+	function customerAnimate(person, personDoor){
+
+		var x = person[0];
 
 		function frame(){
-			x -= 114;
-			person.style.backgroundPositionX = x + 'px';
-			if(x == -228){
+			x -= person[1];
+			personDoor.style.backgroundPositionX = x + 'px';
+			if(x == person[2]){
 				clearInterval(timer);
 			}
 		}
+
 		var timer = setInterval(frame, 350);
 	}
 
 	document.onkeydown = function(event) {
-		console.log(event);
 		if(event.keyCode == 49) {
-			personAnimate(people_1);
-			killPerson();
-			console.log('Должен упасть первый');
+			customerAnimate(eval(roundCustomersArray[0]), customer1);
+			console.log('Shoot in the first door');
 		}
 		else if(event.keyCode == 50) {
-			personAnimate(people_2);
-			killPerson();
-			console.log('Должен упасть второй');
+			customerAnimate(eval(roundCustomersArray[1]), customer2);
+			console.log('Shoot in the second door');
 		}
 		else if(event.keyCode == 51) {
-			personAnimate(people_3);
-			killPerson();
-			console.log('Должен упасть третий');
+			customerAnimate(eval(roundCustomersArray[2]), customer3);
+			console.log('Shoot in the third door');
 		}
 	}
 
-	function gamePlay() {
-		
-		people();
-		openDoors();
+	// Variables
+	var round = null;
+	var clock = 2000;
+	var bankWorkerCount = 3;
+	var roundStatistics = 0;
+	var selectDoorsNumber = null;
 
-		setTimeout(function(){
-			for(var g=1; g<3; g++){
-				if(elemsDoor === 'openelem' + g) {
-					console.log(elemsDoor);
-					var elem = document.getElementById('sprite_' + g);
-					closeDoor(elem);
-					elemsDoor = 'closeelem' + g;
-					console.log(elemsDoor);
-				}
-			}
-		}, 2000);
+	// Doors
+	var door1 = document.getElementById('sprite_1');
+	var door2 = document.getElementById('sprite_2');
+	var door3 = document.getElementById('sprite_3');
+
+	var doorsArray = [ door1, door2, door3, [door1, door2], [door1, door3], [door2, door3], [door1, door2, door3] ];
+	
+	// Customers, Banditos, Crazy kid with bomb
+	var customer1 = document.getElementById('people_1');
+	var customer2 = document.getElementById('people_2');
+	var customer3 = document.getElementById('people_3');	
+
+	var customersArray = [ 'woman', 'man', 'bandit', 'bombBoyBoomer', 'dollarBoyBoomer', 'renegade' ];
+	var roundCustomersArray = new Array(3);
+
+	// Customers data: start position, step, final position, container
+	var woman = [684, 114, 798];
+	var man = [912, 114, 228];
+	var bandit = [0, 114, 228];
+	var bombBoyBoomer = [0, 114, 912];
+	var dollarBoyBoomer = [0, 114, 912];
+	var renegade = [0, 114, 228];
+
+	// Button
+	var startButton = document.getElementById('start');
+
+	function startNewRound() {
+		uploadCustomers();
+		openSelectDoors(selectRandomDoors());
+		setTimeout(output, clock);
+
+		roundStatistics++;
+		console.log('Round ' + roundStatistics + ' is done!');
+	}
+
+	output = function() {
+		if(true) {
+			// ...
+			closeSelectDoors(selectDoorsNumber);
+			setTimeout(restartRound, clock);
+		} else {
+			// ...
+			bankWorkerCount--;
+		}
+	}
+
+	restartRound = function() { 
+		startGame();
+	}
+
+
+	function gameOver() {
+		alert('Game Over');
 	}
 	
-	setInterval(gamePlay, 3000);
-
-	var sexBomBoy = {
-		bomb : 2,
-		money : 200
+	function startGame() {
+		if(bankWorkerCount > 0 && roundStatistics < 12) {
+			roundCustomersArray = new Array(3);
+			startNewRound();
+		} else {
+			gameOver();
+		}	
 	}
-
-	function killPerson() {
-		var smech = 110;
-		if (people == sexBomBoy) {
-			var seek;
-			seek = people_1.style.backgroundPositionY;
-			people_1.style.backgroundPositionY = bomb + smech + 'px';
-			people_2.style.backgroundPositionY = bomb + smech + 'px';
-			people_3.style.backgroundPositionY = bomb + smech + 'px';
-			console.log(seek);
-		}
-
-		if (people == man) {
-			var seek;
-			seek = people_1.style.backgroundPositionY;
-			people_1.style.backgroundPositionY = bomb + smech + 'px';
-			people_2.style.backgroundPositionY = bomb + smech + 'px';
-			people_3.style.backgroundPositionY = bomb + smech + 'px';
-			console.log(seek);
-		}
-	} 
-}
+	
+	startButton.onclick = startGame;
+	
+}(document));
